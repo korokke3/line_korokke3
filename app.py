b13778e7483630b3f1f81d86fe2da8ad36041bb5
korokke3 <70103654+korokke3@users.noreply.github.com>
2025-05-31 00:41:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,12 +87,8 @@ def callback():
     return 'OK'
 
 
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
+import os
 
-    app.run(debug=options.debug, port=options.port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
